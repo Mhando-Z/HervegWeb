55fddf51186b3c1ec01987c9c4bf18d4662f96ed
Mhando-Z <Mhandosz17@gmail.com>
2025-12-11 10:02:48 +0300

    Phase One

--- src/app/aboutus/ourimpact/page.jsx
+++ src/app/aboutus/ourimpact/page.jsx
@@ -77,12 +77,12 @@ const ImpactPage = () => {
     <div className="min-h-screen bg-linear-to-br from-green-50 via-white to-emerald-50">
       {/* Hero Section */}
       <motion.section
-        className="relative overflow-hidden min-h-screen bg-linear-to-r from-emerald-600 to-green-700 text-white py-20 px-6"
+        className="relative overflow-hidden min-h-screen bg-linear-to-r mask-b-from-90% from-emerald-600 to-green-700 text-white py-20 px-6"
         initial="hidden"
         animate="visible"
         variants={fadeInUp}
       >
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-6xl mx-auto min-h-screen items-center justify-center flex flex-col">
           <motion.div variants={fadeInUp} className="text-center">
             <motion.div
               initial={{ scale: 0 }}
