d74b2c3db69d85b68a31b101fec4f9693182f106
Mhando-Z <Mhandosz17@gmail.com>
2025-12-11 11:02:50 +0300

    Phase One

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class page extends Component {
-  render() {
-    return <div>page</div>;
-  }
+function page() {
+  return <div>page</div>;
 }
 
 export default page;
