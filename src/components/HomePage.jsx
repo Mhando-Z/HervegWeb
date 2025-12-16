d39494b6d49f2549a289fe4f7b37003ba3d091e0
Mhando-Z <Mhandosz17@gmail.com>
2025-12-16 20:29:23 +0300

    Phase One

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -408,7 +408,7 @@ const HomePage = () => {
             </p>
           </motion.div>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
             {stories.map((story, index) => (
               <motion.div
                 key={index}
