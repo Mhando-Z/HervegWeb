d70cb8b4bcd71f5716563a66efef68ae4f9ce04b
Mhando-Z <Mhandosz17@gmail.com>
2025-12-28 19:35:55 +0300

    Phase One

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import PromoStats from "@/components/PromoStats";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col overflow-x-hidden items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {/* HomePgae */}
       <HomePage />
       {/* promo section */}
