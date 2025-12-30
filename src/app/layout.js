cb5c4590cd8c9f57894ff2afb29c762fd4c8bcfc
Mhando-Z <Mhandosz17@gmail.com>
2025-12-30 16:02:14 +0300

    Phase One

--- src/app/layout.js
+++ src/app/layout.js
@@ -64,6 +64,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-Q7LL7B97XG"
