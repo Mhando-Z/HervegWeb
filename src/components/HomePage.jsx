583e06e9c5791face0d9645419f3ce8a6e75eab0
Mhando-Z <Mhandosz17@gmail.com>
2026-01-07 10:53:41 +0300

    Phase One

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -360,7 +360,7 @@ const HomePage = () => {
 
       {/* Features & Stories */}
       <section className="py-20 bg-gray-50">
-        <div className="container mx-auto px-6">
+        <div className="xl:container mx-auto px-6">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
